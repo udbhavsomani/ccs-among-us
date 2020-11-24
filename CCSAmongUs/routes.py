a031ad0f9e5a454efb6589f50747d727906963b4
Stark <udbhavsomani18@gmail.com>
2020-11-24 16:42:27 +0530

    correction

--- CCSAmongUs/routes.py
+++ CCSAmongUs/routes.py
@@ -63,7 +63,6 @@ def memberRegister():
 @login_required
 def terminal():
     if request.method == 'POST' and request.form['command'] == 'get_teamname':
-        print(type(current_user.teamname))
         return jsonify({'user': current_user.teamname})
 
     if request.method == 'POST' and request.form['command'] == 'get_coins':
