af318d29030229dc9d5b4fbb361ec745bcb3ef35
Stark <udbhavsomani18@gmail.com>
2020-12-15 23:17:54 +0530

    Final server data commit. Create db before starting the app.

--- CCSAmongUs/__init__.py
+++ CCSAmongUs/__init__.py
@@ -1,1 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '968331509722a24aaca4a6ab62dea0cd'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:****@localhost:5432/codesus'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from CCSAmongUs import routes
